cf3906d1dac99d457d4a7abca5b4b1028075ecf5
Ethan-Hatch <ehatch@utah.gov>
2022-04-14 10:44:45 -0600

    added test folder and script

---
+++ src/test/java/TestFile.java
@@ -0,0 +1,29 @@
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.Test;
+
+public class TestFile
+{
+    public WebDriver driver;
+    public String baseUrl = "https://www.lambdatest.com/";
+
+    @Test
+    public void test1() {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
+        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+        driver.close();
+    }
+}
